bfd4fb5be1e72b056cfff99fa16800962e887c2e
Kamusiime Immaculate <63217020+KamusiiimeImmaculate@users.noreply.github.com>
2023-08-03 17:07:32 +0300

    Add files via upload

---
+++ secrets.h
@@ -0,0 +1,20 @@
+// Use this file to store all of the private credentials 
+// and connection details
+
+//#define SECRET_SSID "ARMTEPRO"		// replace MySSID with your WiFi network name
+//#define SECRET_PASS "#A!skme123?"	// replace MyPassword with your WiFi password
+
+//#define SECRET_SSID "Kiira-Motors-Annex"    // replace MySSID with your WiFi network name
+//#define SECRET_PASS "wifi@kmc20" // replace MyPassword with your WiFi password
+
+//#define SECRET_SSID "Kiira-Motors-Main"    // replace MySSID with your WiFi network name
+//#define SECRET_PASS "wifi@kmc20" // replace MyPassword with your WiFi password
+
+#define SECRET_SSID "ImmyEmos12"    // replace MySSID with your WiFi network name
+#define SECRET_PASS "ImmyK@123" // replace MyPassword with your WiFi password
+
+//#define SECRET_SSID "IntelliSys ProMax"    // replace MySSID with your WiFi network name
+//#define SECRET_PASS "qwerty0976" // replace MyPassword with your WiFi password
+
+//#define SECRET_CH_ID 2021641		// replace 0000000 with your channel number
+//#define SECRET_WRITE_APIKEY "1404JZW6GM07S2PZ"   // replace XYZ with your channel write API Key
